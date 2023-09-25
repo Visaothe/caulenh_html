b354a3007221c5a0cdc8379735f02849d81ac9cc
visaothe <quangthe.nb89@gmail.com>
2023-09-25 22:55:23 +0700

    luu file

--- js/dondathang.js
+++ js/dondathang.js
@@ -1,2 +1,15 @@
-function x = +document.getElementById("ip1").value;
-function y = +document.getElementById("ip2").value;
+function maney(){
+    const sp = +document.getElementById("ip2").value;
+          if (sp == ""){
+              alert("Nhap thong tin san pham");
+          }
+    const dg = +document.getElementById("ip4").value;
+          if (dg == ""){
+              alert("Nhap thong tin don gia");
+          }
+            if(dg >= 0) {
+                alert("Don gia phai lon hon 0");
+            }
+    const kq = +document.getElementById("ip1").value;
+            document.getElementById("dg").innerHTML="ket qua:" + dg * kq;
+}
